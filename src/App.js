4291a433e4bbfd681c29e24f5c09cfad548e314c
ugurduyar <ismailugurduyar@gmail.com>
2020-06-14 20:45:28 +0300

    changed to class base component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello From React</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello From React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
