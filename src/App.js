31481d1893b8bee2d6974c8c235edf08b3336b66
ugurduyar <ismailugurduyar@gmail.com>
2020-06-14 22:04:27 +0300

    loading conditionals

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    const name = "John Doe";
+    const loading = true;
+
     return (
       <div className="App">
-        <h1>Hello From React</h1>
+        <h1>My App</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {name}</h1>}
       </div>
     );
   }
