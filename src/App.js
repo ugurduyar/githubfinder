91617df5e347cc683134c99fda9b3edb119b676e
ugurduyar <ismailugurduyar@gmail.com>
2020-06-15 15:44:32 +0300

    props passed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar title="Github Finder" icon="fab fa-github" />
       </div>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,9 @@ export class Navbar extends Component {
   render() {
     return (
       <nav className="navbar bg-primary">
-        <h1>Navbar</h1>
+        <h1>
+          <i className={this.props.icon} /> {this.props.title}
+        </h1>
       </nav>
     );
   }
