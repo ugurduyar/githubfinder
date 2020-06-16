eebfab5718b89161a0bb6dec47ef300ff0add45a
ugurduyar <ismailugurduyar@gmail.com>
2020-06-16 11:33:57 +0300

    users fetched from api

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./Useritem";
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        url: "https://api.github.com/users/mojombo",
-      },
-      {
-        id: 2,
-        login: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        url: "https://api.github.com/users/defunkt",
-      },
-      {
-        id: 3,
-        login: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        url: "https://api.github.com/users/pjhyett",
-      },
-    ],
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
