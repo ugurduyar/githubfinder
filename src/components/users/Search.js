fae4666e930209291e1c85922634457c19c5222f
ugurduyar <ismailugurduyar@gmail.com>
2020-06-16 12:44:56 +0300

    search submit added

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -5,11 +5,15 @@ export class Search extends Component {
     text: "",
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.text);
+  };
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
   render() {
     return (
       <div>
-        <form className="form">
+        <form onSubmit={this.onSubmit} className="form">
           <input
             type="text"
             name="text"
