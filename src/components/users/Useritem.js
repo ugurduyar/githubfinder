cbdddb9011a2b1bc9d484a01d69a28ec11a4d1f6
ugurduyar <ismailugurduyar@gmail.com>
2020-06-16 10:04:50 +0300

    more users and styling added

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,24 +1,20 @@
 import React, { Component } from "react";
 
 class Useritem extends Component {
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo",
-  };
   render() {
+    const { login, avatar_url, html_url } = this.props.user;
+
     return (
       <div className="card text-center">
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt=""
           className="round-img"
           style={{ width: "60px" }}
         />
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
         <div>
-          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
             {" "}
             More
           </a>
