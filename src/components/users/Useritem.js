64050cdbd9110080608a163ced2eeba62e19c9c7
ugurduyar <ismailugurduyar@gmail.com>
2020-06-16 16:56:28 +0300

    user page design done

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 const Useritem = ({ user: { login, avatar_url, html_url } }) => {
   return (
     <div className="card text-center">
@@ -11,10 +13,10 @@ const Useritem = ({ user: { login, avatar_url, html_url } }) => {
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           {" "}
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
