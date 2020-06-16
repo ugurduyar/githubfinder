e376960b930898da45e320ab1671749b5cb24aa1
ugurduyar <ismailugurduyar@gmail.com>
2020-06-16 09:49:09 +0300

    first user added

--- src/components/users/Useritem.js
+++ src/components/users/Useritem.js
@@ -1,8 +1,30 @@
 import React, { Component } from "react";
 
 class Useritem extends Component {
+  state = {
+    id: "id",
+    login: "mojombo",
+    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo",
+  };
   render() {
-    return <div>UserItem</div>;
+    return (
+      <div className="card text-center">
+        <img
+          src={this.state.avatar_url}
+          alt=""
+          className="round-img"
+          style={{ width: "60px" }}
+        />
+        <h3>{this.state.login}</h3>
+        <div>
+          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+            {" "}
+            More
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
