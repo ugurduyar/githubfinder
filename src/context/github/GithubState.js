de291e66f0a604e56b6a7e24e7b4cc39d167821f
ugurduyar <ismailugurduyar@gmail.com>
2020-06-17 17:54:42 +0300

    redux

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -21,6 +21,17 @@ const GithubState = (props) => {
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   // Search users
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
 
   // Get user
 
@@ -29,6 +40,7 @@ const GithubState = (props) => {
   // Clear users
 
   // Set loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
   return (
     <GithubContext.Provider
@@ -37,6 +49,7 @@ const GithubState = (props) => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
       }}
     >
       {props.children}
