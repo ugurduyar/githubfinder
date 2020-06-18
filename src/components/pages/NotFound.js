3b876e14939673f359fa4b753b3802ea71852748
ugurduyar <ismailugurduyar@gmail.com>
2020-06-18 13:40:49 +0300

    notfound page added.

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className="lead"> Page not found...</p>
+    </div>
+  );
+};
+
+export default NotFound;
