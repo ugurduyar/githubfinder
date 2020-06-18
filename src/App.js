7454bdc190d6c8a138d0cdabed1a2c47316c1ff7
ugurduyar <ismailugurduyar@gmail.com>
2020-06-18 11:24:50 +0300

    users now showing app level state

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ const App = () => {
                       showClear={users.length > 0 ? true : false}
                       setAlert={showAlert}
                     />
-                    <Users loading={loading} users={users} />
+                    <Users />
                   </Fragment>
                 )}
               />
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import UserItem from "./Useritem";
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
+const Users = () => {
+  const githubContext = useContext(GithubContext);
 
-const Users = ({ users, loading }) => {
+  const { loading, users } = githubContext;
+
   if (loading) {
     return <Spinner />;
   } else {
